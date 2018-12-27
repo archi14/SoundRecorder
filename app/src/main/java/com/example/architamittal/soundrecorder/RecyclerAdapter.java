9fa3cc79866d650b93ed6515934e913fe9b6f756
archita Mittal <archita810@gmail.com>
2018-12-27 16:16:59 +0530

    playing from list successful

--- app/src/main/java/com/example/architamittal/soundrecorder/RecyclerAdapter.java
+++ app/src/main/java/com/example/architamittal/soundrecorder/RecyclerAdapter.java
@@ -1,14 +1,21 @@
 package com.example.architamittal.soundrecorder;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
@@ -31,8 +38,39 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.RecyclerViewHolder holder, int position) {
-        SoundFile file = files.get(position);
+        final SoundFile file = files.get(position);
         holder.textView.setText(file.getFilename());
+        holder.listplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String mFileName = file.getFilePath()+file.getFilename();
+                Log.d("Adapter", file.getFilePath());
+                Uri uri = Uri.parse(file.getFilePath());
+                Log.d("AdapterUri", uri.toString());
+                //Log.d("Adapter", file.getFilePath());
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(file.getFilePath());
+                    mediaPlayer.prepareAsync();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mediaPlayer.start();
+                    }
+                });
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mediaPlayer.reset();
+                        mediaPlayer.release();
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -42,9 +80,13 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
 
     public class RecyclerViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
+        ImageButton listplay;
         public RecyclerViewHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.text);
+            listplay = itemView.findViewById(R.id.listplay);
         }
+
     }
+
 }
